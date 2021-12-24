25bbb082d73a94501048f0f9ed9e7da3e951869c
omerwyo <omerbaggia123@gmail.com>
2021-12-24 12:19:31 +0800

    OOP concepts examples

---
+++ Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String title;
+
+    public Book(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+}
--- BookTest.java
+++ BookTest.java
@@ -1,6 +1,6 @@
 public class BookTest{
     public static void main(String[] args){
-        Book book1 = new Book();
+        Book book = new Book("Harry Potter and the Goblet of Fire");
         book.setTitle("Harry Potter and the Chamber of Secrets");
     }
 }
