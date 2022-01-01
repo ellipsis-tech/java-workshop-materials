c11f1f6dfb65344ddd22741d70ba48bc92528ab5
omerwyo <omerbaggia123@gmail.com>
2022-01-01 22:23:24 +0800

    book

--- Book.java
+++ Book.java
@@ -1,8 +1,10 @@
 public class Book {
     private String title;
+    private String author;
 
-    public Book(String title){
+    public Book(String title, String author){
         this.title = title;
+        this.author = author;
     }
 
     public String getTitle(){
@@ -11,5 +13,13 @@ public class Book {
 
     public void setTitle(String title){
         this.title = title;
+    }
+    
+    public String getAuthor(){
+        return author;
+    }
+    
+    public void setAuthor(String author){
+        this.author = author;
     }
 }
