ae3e1cf204a81d8134add9ecb08bfc1c81e7e6c8
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2024-01-11 19:10:31 +0800

    Reordered files for workshop 2024

---
+++ examples/part5-java-collections/CollectionsInJava.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class CollectionsInJava {
+    public static void main(String[] args) {
+        ArrayList<Person> al = new ArrayList<>();
+        al.add(new Person("Sally", 20));
+        al.add(new Person("Ahmad", 23));
+
+        Iterator<Person> iter = al.iterator();
+
+        System.out.println(al);
+
+        
+    }
+}
+
+class Person{
+    String name;
+    int age;
+
+    public Person(String name, int age){
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;  
+    }
+
+    public int getAge(){
+        return age;
+    }
+    
+    @Override
+    public String toString(){
+        return "Person( name=" + name +", age=" + age + " )";
+    }
+}
